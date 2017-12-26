48c31ac59f8d983ace37cdd4a32ae92d8f63adfd
Giraffa <tkdhi0107@naver.com>
2017-12-26 13:30:22 +0900

    MemberManager List에서 Map<String, Member>로 수정(String은 id) Server 회원가입시 id,pw,phone,address string으로 받고, 로그인도 String으로 받음.

--- prj/src/server/Server.java
+++ prj/src/server/Server.java
@@ -1,13 +1,21 @@
 package server;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import client.Member;
+import master.Menu;
+import master.MenuSFM;
 
 public class Server {
 	public static final int REGISTER = 1;
@@ -15,6 +23,9 @@ public class Server {
 	public static File target = new File("files", "menus.db");
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 
+		MenuSFM.menuLoad();
+		MenuSFM.menuPrintConsole();
+		
 		//소켓연결
 		Socket socket = new ServerSocket(20000).accept();
 		
@@ -22,44 +33,40 @@ public class Server {
 		ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
 		ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream()); 
 		
-		
 		//회원가입, 로그인
 		while(true) {
 			int select = in.readInt();
 			System.out.println("선택 = "+ select);
 			//회원가입
 			if(select == REGISTER) {
-				Member m = (Member) in.readObject();
-				boolean result = MemberManager.register(m.getId(), m.getPwd());
-				out.writeInt(REGISTER); out.flush();
+//				Member m = (Member) in.readObject();
+				String id = in.readUTF();
+				String pw = in.readUTF();
+				String phone = in.readUTF();
+				String address = in.readUTF();
+				System.out.println(id);
+				System.out.println(pw);
+				System.out.println(phone);
+				System.out.println(address);
+				boolean result = MemberManager.register(id, pw, phone, address);
 				out.writeBoolean(result); out.flush();
 			//로그인
 			}else if(select == LOGIN) {
-				Member m = (Member) in.readObject();
-				boolean result = MemberManager.login(m.getId(), m.getPwd());
+				String id = in.readUTF();
+				String pw = in.readUTF();
+				boolean result = MemberManager.login(id, pw);
 				System.out.println(result);	//로그인 성공여부 체크
 				out.writeBoolean(result); out.flush();
-				
 				//로그인 성공하면
 				if(result) {
-				
+					System.out.println("로그인 성공");
 				}
-			}	
+			}
 			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-		
 		}
+		
+		
+		
 		
 	}
 }
