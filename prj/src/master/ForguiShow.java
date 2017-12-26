46e2e1c6f8b3781752e58d04da9c0b3dd81c78cc
pallenJ <pallennellap17@gmail.com>
2017-12-26 11:41:28 +0900

    Forgui switch 내용 메소드화

--- prj/src/master/ForguiShow.java
+++ prj/src/master/ForguiShow.java
@@ -16,7 +16,7 @@ public class ForguiShow extends JFrame {
 	private JButton[] bts = new JButton[USING.length];// button을 모아놓은 Array USING과 당연히 length가 같음
 
 	public ForguiShow() {// 강사님의 기본 틀대로 만든 JFrame 상속 클래스
-		
+
 		mains();
 		event();
 		menu();
@@ -31,7 +31,7 @@ public class ForguiShow extends JFrame {
 	}
 
 	private void mains() {
-		
+
 		lb.setHorizontalAlignment(SwingConstants.CENTER);
 		setContentPane(bg);// bg를 배경에 설치하라
 
@@ -90,119 +90,33 @@ public class ForguiShow extends JFrame {
 		switch (key) {
 		case "메뉴보기":
 			MenuSFM.menuPrintConsole();// 콘솔창에서 메뉴목록 확인가능
-			new MenuBoard();//새창에서 메뉴 보여줌
+			new MenuBoard();// 새창에서 메뉴 보여줌
 			break;
 
 		case "메뉴추가":
-			System.out.print("메뉴분류:");
-			group = JOptionPane.showInputDialog("분류 입력");// JOptionPane로 입력 받음
+			menuEdit(true);
 
-			if (group == null || group.equals("")) {// 취소버튼을 누르거나 아무것도 넣지않고 확인을 누를때 자동 취소
-				System.out.println("취소");
-				break;
-			}
-
-			if (!MenuSFM.getGroupString().contains(group)) {// 메뉴 목록에 있는 분류인지 검사후 없으면 경고창출력
-				System.out.println(MenuSFM.WARNING[1]);
-				JOptionPane.showMessageDialog(null, MenuSFM.WARNING[1], "", JOptionPane.ERROR_MESSAGE);
-				break;
-			}
-
-			System.out.println(group);
-
-			System.out.print("메뉴이름:");
-
-			name = JOptionPane.showInputDialog("메뉴이름");
-			System.out.println(name);
-			if(MenuSFM.getMenu(name)!=null) {//이미 있는 메뉴인지 검사후 있면 경고창출력
-				System.out.println(MenuSFM.WARNING[4]);
-				JOptionPane.showMessageDialog(null, MenuSFM.WARNING[1], "", JOptionPane.ERROR_MESSAGE);
-				break;
-			}
-			
-			System.out.print("가격:");
-			
-			String str=JOptionPane.showInputDialog("가격");
-			if (str == null || str.equals("")) {// 취소버튼을 누르거나 아무것도 넣지않고 확인을 누를때 자동 취소
-				System.out.println("취소");
-				break;
-			}
-
-			try {
-				price = Integer.parseInt(str);
-			} catch (Exception e) {
-				System.out.println(MenuSFM.WARNING[0]);
-				JOptionPane.showMessageDialog(null, "ERROR", "", JOptionPane.ERROR_MESSAGE);
-				break;
-				// 정수 이외의 것을 출력할경우 예외처리후 멈춤
-			}
-
-			System.out.println(price);
-			MenuSFM.addMenu(group, new Menu(name, price));
-			
 			break;
-		// JOptionPane으로 입력 -> 제대로 입력됐는지 검사 ->콘솔창에 검사출력&예외가 발생하면 메세지 날리기의 반복.
-		// 아무 문제 없으면 기능 수행. 다른 기능도 마찬가지
-		//MenuSFM	
 		case "메뉴삭제":
-			System.out.print("메뉴분류:");
-			group = JOptionPane.showInputDialog("메뉴 분류");
-			if (group == null || group.equals("")) {
-				System.out.println("취소");
-				break;
-			}
-			if (!MenuSFM.getGroupString().contains(group)) {
-				System.out.println(MenuSFM.WARNING[1]);
-				JOptionPane.showMessageDialog(null, MenuSFM.WARNING[1], "", JOptionPane.ERROR_MESSAGE);
-				break;
-			}
-			System.out.println(group);
-			System.out.print("메뉴이름:");
-
-			name = JOptionPane.showInputDialog("메뉴 이름");
-
-			if (name == null || name.equals("")) {
-				System.out.println("취소");
-				break;
-			}
-
-			System.out.println(name);
-			// 메뉴추가와 거의 동일
-			MenuSFM.removeMenu(group, name);// 메뉴 삭제 메소드
-											// 이름만으로 메뉴를 찾으므로 가격 입력 불필요
+			menuEdit(false);
 			break;
 
 		case "분류추가":
-			System.out.print("분류명:");
-
-			group = JOptionPane.showInputDialog("메뉴 분류");// JOptionPane으로 입력
-
-			if (group == null || group.equals("")) {// 마찬가지로 입력을 하지 않고 확인을 누르거나 아니면 그냥 취소버튼을 누르면 종료
-				System.out.println("취소");
-				break;
-			}
-			System.out.println(group);
-			MenuSFM.addMenu(group);// 분류 추가 메소드
+			groupEdit(true);
 
 			break;
 
 		case "분류삭제":
-			System.out.print("분류명:");
-			group = JOptionPane.showInputDialog("메뉴 분류");
+			groupEdit(false);
 
-			if (group == null || group.equals("")) {// 전전의 주석과 같음
-				System.out.println("취소");
-				break;
-			}
-			System.out.println(group);
-			MenuSFM.removeMenu(group);// 분류 삭제 메소드
 			break;
 
 		case "메뉴리셋":
-			int choice = JOptionPane.showConfirmDialog(null, "정말 리셋 하시겠습니까?", "",
-					JOptionPane.OK_CANCEL_OPTION,JOptionPane.WARNING_MESSAGE, null);
-			if (choice == JOptionPane.CANCEL_OPTION)break;
-			
+			int choice = JOptionPane.showConfirmDialog(null, "정말 리셋 하시겠습니까?", "", JOptionPane.OK_CANCEL_OPTION,
+					JOptionPane.WARNING_MESSAGE, null);
+			if (choice == JOptionPane.CANCEL_OPTION)
+				break;
+
 			MenuSFM.menuReset();
 			System.out.println("리셋완료");
 			JOptionPane.showMessageDialog(null, "리셋완료");
@@ -218,5 +132,85 @@ public class ForguiShow extends JFrame {
 			System.out.println(MenuSFM.WARNING[0]);
 			break;
 		}
+	}
+
+	private static void menuEdit(boolean flag) {// 메뉴 추가/삭제용  sysout은 오직 콘솔확인용
+		String group = "";//메뉴 분류
+		String name = "";//메뉴 이름
+		
+		System.out.print("메뉴분류:");
+		
+		group = JOptionPane.showInputDialog("분류 입력");// JOptionPane로 입력 받음
+
+		if (group == null || group.equals("")) {// 취소버튼을 누르거나 아무것도 넣지않고 확인을 누를때 자동 취소
+			System.out.println("취소");
+			return;
+		}
+
+		if (!MenuSFM.getGroupString().contains(group)) {// 메뉴 목록에 있는 분류인지 검사후 없으면 경고창출력
+			System.out.println(MenuSFM.WARNING[1]);
+			JOptionPane.showMessageDialog(null, MenuSFM.WARNING[1], "", JOptionPane.ERROR_MESSAGE);
+			return;
+		}
+
+		System.out.println(group);
+
+		System.out.print("메뉴이름:");
+
+		name = JOptionPane.showInputDialog("메뉴이름");
+		System.out.println(name);
+
+		if (name == null || name.equals("")) {// 취소버튼을 누르거나 아무것도 넣지않고 확인을 누를때 자동 취소
+			System.out.println("취소");
+			return;
+		}
+
+		if (flag) {//flag가 true면 메뉴추가
+			//바로 MenuSFM.addMenu로 가도 상관 없으나 그러면 입력중 에러가 나도 알지못함. 입력을 다하고 나서야 알 수 있음. 따라서 편의성을 위해 아래처럼 처리
+			int price = 0;//가격
+			if (MenuSFM.getMenu(name) != null) {// 이미 있는 메뉴인지 검사후 있면 경고창출력
+				System.out.println(MenuSFM.WARNING[4]);
+				JOptionPane.showMessageDialog(null, MenuSFM.WARNING[1], "", JOptionPane.ERROR_MESSAGE);
+				return;//이미 있는 메뉴면 멈춤
+			}
+
+			System.out.print("가격:");
+
+			String str = JOptionPane.showInputDialog("가격");
+			if (str == null || str.equals("")) {// 취소버튼을 누르거나 아무것도 넣지않고 확인을 누를때 자동 취소
+				System.out.println("취소");
+				return;//똑같이 입력한게 없으면 자동취소
+			}
+
+			try {
+				price = Integer.parseInt(str);//정수 이외의 것은 에러메세지 출력후 취소
+			} catch (Exception e) {
+				System.out.println(MenuSFM.WARNING[0]);
+				JOptionPane.showMessageDialog(null, "ERROR", "", JOptionPane.ERROR_MESSAGE);
+				return;
+			}
+
+			System.out.println(price);
+			MenuSFM.addMenu(group, new Menu(name, price));
+		} else {
+			MenuSFM.removeMenu(group, name);//MenuSFM.removeMenu에서 모든걸 처리
+		}
+
+	}
+
+	private static void groupEdit(boolean flag) {// 그룹 추가/삭제용
+		String group = "";
+		System.out.print("분류명:");
+		group = JOptionPane.showInputDialog("메뉴 분류");
+
+		if (group == null || group.equals("")) {// 전전의 주석과 같음
+			System.out.println("취소");
+			return;
+		}
+		System.out.println(group);
+		if (flag)
+			MenuSFM.addMenu(group);//true면 추가
+		else
+			MenuSFM.removeMenu(group);//false면 삭제
 	}
 }
