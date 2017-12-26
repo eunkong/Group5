dbc3b5279328f641b2fb1c0a169c52d4f370bd53
eunkong <dmsrud7280@nave.com>
2017-12-26 13:20:52 +0900

    Client 클래스 수정 - 회원가입시 전화번호, 주소 추가 - 로그인 성공시 MenuSFM에서 메뉴 출력 Member 클래스 수정 - 변수 추가 (전화번호, 주소, 등급, 포인트, 총 주문 횟수)

--- prj/src/client/Client.java
+++ prj/src/client/Client.java
@@ -1,7 +1,5 @@
 package client;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -9,46 +7,50 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
+import master.MenuSFM;
+
 public class Client {
-	private static final int REGISTER = 1;
-	private static final int LOGIN = 2;
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		int port = 20000;
 		InetAddress inet = InetAddress.getByName("192.168.0.243");
 		Socket socket = new Socket(inet, port);
-		ObjectOutputStream out = new ObjectOutputStream(new BufferedOutputStream(socket.getOutputStream()));
-		System.out.println("로그인하세요");
-//		ObjectInputStream in = new ObjectInputStream(new BufferedInputStream(socket.getInputStream()));
-//		System.out.println("전송준비");
+		ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
+		//in 만들때 ObjectInputStream(BufferedInputStream(socket.getInputStream()));
+		//하면 더이상 진행되지 않음. (왜..?)
+		ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
+		System.out.println("전송준비");
 		Scanner s = new Scanner(System.in);
 		
 		while(true) {
 			System.out.print("1.회원가입 or 2.로그인 or 3.종료 : ");
 			int menu = s.nextInt();
-			s.nextLine();
 			if(menu==3) break;
+			s.nextLine();
 			out.writeInt(menu);
 			out.flush();
-			System.out.println("int 전송");
+			// System.out.println("int 전송");
 			
 			System.out.print("ID : ");
-			String id = s.nextLine();
+			out.writeUTF(s.nextLine()); out.flush(); //id를 server에 넘김
 			System.out.print("PWD : ");
-			String pwd = s.nextLine();
+			out.writeUTF(s.nextLine()); out.flush(); //pwd를 server에 넘김
+			System.out.print("전화번호  : ");
+			out.writeUTF(s.nextLine()); out.flush(); //phoneNumber를 server에 넘김
+			System.out.print("주소 : ");
+			out.writeUTF(s.nextLine()); out.flush(); //address를 server에 넘김
+
+			boolean result = in.readBoolean();
+			Member my = (Member)in.readObject();
 			
-			Member master = new Member(id, pwd);
-			out.writeObject(master);
-			out.flush();
-			System.out.println("master 전송");
-			break;
-			
-//			boolean result = in.readBoolean();
-//			System.out.println("결과 : " + result);
-			
-//			if(result==false) continue;
+			if(menu==2 && result==true) {
+				System.out.println("메뉴출력");
+				MenuSFM.menuLoad();
+				MenuSFM.menuPrintConsole();
+			}
+			if(result==false) continue;
 		}
 		s.close();
-//		in.close();
+		in.close();
 		out.close();
 		System.out.println("종료");
 		
--- prj/src/client/Member.java
+++ prj/src/client/Member.java
@@ -3,12 +3,18 @@ package client;
 import java.io.Serializable;
 
 public class Member implements Serializable {
-	private String id;
-	private String pwd;
-	public Member(String id, String pwd) {
+	private String id, pwd, phoneNumber, address, grade;
+	private String[] gradeArray = {"¿œπ›", "VIP", "VVIP"};
+	private int orderCount, point;
+	public Member(String id, String pwd, String phoneNumber, String address) {
 		super();
 		this.id = id;
 		this.pwd = pwd;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+		this.grade = gradeArray[0];
+		this.orderCount = 0;
+		this.point = 0;
 	}
 	public String getId() {
 		return id;
@@ -22,5 +28,36 @@ public class Member implements Serializable {
 	public void setPwd(String pwd) {
 		this.pwd = pwd;
 	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public int getOrderCount() {
+		return orderCount;
+	}
+	public void setOrderCount(int orderCount) {
+		this.orderCount = orderCount;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	
 	
 }
