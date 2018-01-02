5d1d6de64dd51503341eb359d37c0f1e6f1e7112
pallenJ <pallennellap17@gmail.com>
2018-01-02 09:23:29 +0900

    .

---
+++ prj/src/gui/MemIdx.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package gui;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.Map;
+
+import client.*;
+/**
+ * @author 204-26
+ * prj
+ * @param
+ */
+public class MemIdx {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 * @throws FileNotFoundException 
+	 * @throws ClassNotFoundException 
+	 */
+	public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
+		// TODO Auto-generated method stub
+		ObjectInputStream in = new ObjectInputStream(
+				new BufferedInputStream(
+						new FileInputStream(new File("files", "memberlist.db"))));
+		Map<String, Member> map = (Map<String, Member>) in.readObject();
+		for (String string : map.keySet()) {
+			System.out.println("id:"+string);
+			System.out.println("pw:"+map.get(string).getPwd());
+			System.out.println();
+		}
+		in.close();
+		
+	}
+
+}
