fe38c7e926fd3c9742dbdb49e641e9457132a541
Giraffa <tkdhi0107@naver.com>
2018-01-03 11:29:52 +0900

    serverGui 패키지 생성 : server측 그래픽 구현 서버 메인, 종속화면 생성.
    메인화면 – 종속화면 연결

---
+++ prj/src/serverGui/ServerMainFrame.java
@@ -0,0 +1,102 @@
+package serverGui;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.*;
+
+import master.ForguiShow;
+import master.MenuSFM;
+
+public class ServerMainFrame extends JFrame{
+	private JPanel bg = new JPanel(new BorderLayout());	//패널 만들기.
+	private JLabel lb1 = new JLabel("사장님꺼~♥");
+	private JLabel lb2 = new JLabel("이건");
+	private JButton bt1 = new JButton("주문관리");
+	private JButton bt2 = new JButton("고객관리");
+	private JButton bt3 = new JButton("메뉴관리");
+	private JButton bt4 = new JButton("나가자");
+
+	private OrderManageWindow orderWindow = new OrderManageWindow(this,true);		//boolean은 창 종속 매개변수
+	private GuestManageWindow guestWindow = new GuestManageWindow(this,true);
+	
+	public ServerMainFrame() {
+		design();
+		event();
+		menu();
+		setTitle("사장님꺼~♥");
+		setSize(482, 343);
+		setResizable(false);		//크기변경 불가
+		setLocationByPlatform(true);	//위치를 운영체제가 정하도록 설정, 여러개 창켜면 알아서 위치 겹치지 않게 만듬.
+		setVisible(true);
+	}
+
+	private void menu() {
+		
+	}
+
+	private void event() {
+		bt1.addActionListener(e->{
+			//주문관리창 불러오기
+			orderWindow.setVisible(true);
+		});
+		
+		bt2.addActionListener(e->{
+			//고객관리창 불러오기
+			guestWindow.setVisible(true);
+		});
+		
+		bt3.addActionListener(e->{
+			MenuSFM.menuLoad();//files에서 menu목록 가져와서 MenuSFM에 재저장(내용을 읽어옴)
+			new ForguiShow();//GUI로 메뉴 관리시작
+		});
+		
+		bt4.addActionListener(e->{
+			System.exit(0);
+		});
+	}
+
+	private void design() {
+		setContentPane(bg);  //bg를 배경에 설치하라.
+		bg.setLayout(null);
+		bg.setBackground(Color.WHITE);
+		
+		bt1.setBounds(304, 73, 133, 40);
+		bt1.setForeground(Color.darkGray);
+		bt1.setBackground(Color.orange);
+		bg.add(bt1);
+		
+		bt2.setBounds(304, 123, 133, 40);
+		bt2.setForeground(Color.darkGray);
+		bt2.setBackground(Color.orange);
+		bg.add(bt2);
+		
+		bt3.setBounds(304, 172, 133, 40);
+		bt3.setForeground(Color.darkGray);
+		bt3.setBackground(Color.orange);
+		bg.add(bt3);
+		
+		bt4.setBounds(304, 222, 133, 40);
+		bt4.setForeground(Color.darkGray);
+		bt4.setBackground(Color.orange);
+		bg.add(bt4);
+		
+		lb1.setForeground(new Color(135, 206, 250));
+		lb1.setFont(new Font("궁서체", Font.ITALIC, 40));
+		lb1.setBounds(12, 86, 285, 71);
+		bg.add(lb1);
+		
+		lb2.setForeground(new Color(30, 144, 255));
+		lb2.setFont(new Font("궁서체", Font.ITALIC, 40));
+		lb2.setBounds(12, 36, 285, 71);
+		bg.add(lb2);
+		
+	}
+}
+
+//public class ServerMainFrame {
+//	public static void main(String[] args) {	
+//		new ServerMainFrame();
+//	}
+//}
